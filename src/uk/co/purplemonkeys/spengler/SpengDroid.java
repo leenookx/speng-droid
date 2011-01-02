ba78b8b16d231ed6f4e777df4ab15b2766db69ec
Leenookx <lnookx@googlemail.com>
2011-01-02 18:36:50 +0000

    Added a basic preferences menu option.

--- src/uk/co/purplemonkeys/spengler/SpengDroid.java
+++ src/uk/co/purplemonkeys/spengler/SpengDroid.java
@@ -1,7 +1,11 @@
 package uk.co.purplemonkeys.spengler;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class SpengDroid extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +13,27 @@ public class SpengDroid extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.spengdroid, menu);
+        return true;
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) 
+    {
+    	switch (item.getItemId()) 
+    	{
+    		case R.id.preferences_menu_id:
+    			startActivity(new Intent(this, Preferences.class));
+    			return true;
+			default:
+				break;
+    	}
+
+    	return super.onOptionsItemSelected(item);
     }
 }
